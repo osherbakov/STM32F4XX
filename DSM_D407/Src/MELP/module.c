b6e30e51b877a37e804e8e93ef6a66b196f94656
Oleg Sherbakov <olegsher@sollecon.com>
2015-09-24 11:33:06 -0700

    Minor change

--- DSM_D407/Src/MELP/module.c
+++ DSM_D407/Src/MELP/module.c
@@ -55,7 +55,7 @@ BSP_LED_On(LED5);
 		melp_syn(&melp_syn_par, speech);
 		arm_scale_f32(speech, 1.0f/32768.0f, pDataOut, MELP_FRAME_SIZE);		
 BSP_LED_Off(LED5);
-		v_equ(pDataOut, pDataIn, MELP_FRAME_SIZE);		
+//		v_equ(pDataOut, pDataIn, MELP_FRAME_SIZE);		
 		pDataIn += MELP_FRAME_SIZE * 4;
 		pDataOut += MELP_FRAME_SIZE * 4;
 		*pInSamples -= MELP_FRAME_SIZE;
